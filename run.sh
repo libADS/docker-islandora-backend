3f01469a1a4fca7131dce7fb70e58883b2844b11
Mark Cooper <markchristophercooper@gmail.com>
2014-09-12 19:49:13 -0700

    Only run backend

--- run.sh
+++ run.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# assumes not running currently
-docker run -d -p 3306:3306 --name mysql -e MYSQL_PASS="admin" tutum/mysql
-# pause long enough for db to become available - may need to up value
-sleep 10
 docker run -d -p 8080:8080 --name fedora --link mysql:db islandora/backend:latest
 echo "Islandora backend requires ~ 2 minutes to initialize ..."
 
